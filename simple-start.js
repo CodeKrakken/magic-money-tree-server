735e367931a1835d07c3fcee8d215fab89f1bdac
CodeKrakken <newholm1@hotmail.com>
2021-06-07 20:37:19 +0100

    Switching workspaces

--- simple-start.js
+++ simple-start.js
@@ -8,6 +8,10 @@ const binanceClient = new ccxt.binance({
   // 'enableRateLimit': true,
 });
 
+// console.log(binanceClient)
+
+
+
 const config = {
   asset: 'ETH',
   base: 'USDT',
@@ -15,18 +19,13 @@ const config = {
   fee: 0.076
 };
 
-async function fetchPriceHistory(symbol, timeframe) {
-  let data = await binanceClient.fetch_ohlcv(symbol, timeframe)
-  return data
-}
-
 async function all(coinPairs, timeframe) {
   let allHistory = []
   for (let i = 0; i < coinPairs.length; i++) {
     let coinPair = coinPairs[i].replace('/', '')
     try {
       console.log(`${i+1}/${coinPairs.length} Fetching price history for ${coinPair}`)
-      let historyRaw = await axios.get(`https://api.binance.com/api/v1/klines?symbol=${coinPair}&interval=1m`)
+      let historyRaw = await axios.get(`https://api.binance.com/api/v1/klines?symbol=${coinPair}&interval=${timeframe}`)
       allHistory.push({
         symbol: coinPair,
         history: historyRaw
@@ -44,6 +43,10 @@ async function run() {
   const exchangeInfo = await binanceClient.load_markets()
   console.log("setting coinpairs")
   const coinPairs = Object.keys(exchangeInfo).filter(pair => pair.includes(config.base))
+  console.log('getting markets')
+  const markets = await binanceClient.fetch_markets()
+  console.log(markets)
+  console.log(exchangeInfo)
   console.log('getting price history')
   const allPriceHistory = await all(coinPairs, '1m')
   console.log('Got price history')
