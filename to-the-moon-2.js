54583d64804b53d6b2335328269e80e4c5e6892e
CodeKrakken <newholm1@hotmail.com>
2021-06-20 12:06:47 +0100

    Identify bullish (and bearish) markets for active currency

--- to-the-moon-2.js
+++ to-the-moon-2.js
@@ -14,11 +14,10 @@ const binance = new ccxt.binance({
 });
 
 let wallet = {
-  'USDT': 2000  
+  'BTC': 2000  
 }
 
 let currentMarket = 'None'
-let currentPrice = 0
 let currentAsset = 'None'
 let currentBase = 'USDT'
 let boughtPrice = 0
@@ -32,8 +31,9 @@ async function run() {
 async function tick() {
   let activeCurrency = await getActiveCurrency()
   console.log(activeCurrency)
-  let markets = await getMarkets(activeCurrency)
-  console.log(markets)
+  let marketNames = await getMarkets(activeCurrency)
+  let bullishMarkets = await getBullishMarkets(marketNames, activeCurrency)
+  console.log(bullishMarkets)
 }
 
 async function getActiveCurrency() {
@@ -149,4 +149,136 @@ async function fetchDollarVolume(symbol) {
     return 'Invalid market'
   }
 }
+
+async function getBullishMarkets(marketNames, activeCurrency) {
+  try {
+    let exchangeHistory = await fetchAllHistory(marketNames)
+    let bullishMarkets = await filter(exchangeHistory, activeCurrency)
+    return bullishMarkets
+  } catch (error) {
+    getBullishMarkets()
+  }
+}
+
+async function fetchAllHistory(marketNames) {
+  let n = marketNames.length
+  let returnArray = []
+  console.log(`Fetching History @ ${timeNow()}\n`)
+  for (let i = 0; i < n; i ++) {
+    try {
+      let marketName = marketNames[i]
+      let symbolName = marketName.replace('/', '')
+      let symbolHistory = await fetchOneHistory(symbolName)  
+      let symbolObject = {
+        'history': symbolHistory,
+        'market': marketName
+      }
+      symbolObject = await collateData(symbolObject)
+      await returnArray.push(symbolObject)
+    } catch (error) {
+      marketNames.splice(i, 1)
+      i --
+      n --
+    }
+  }
+  return returnArray
+}
+
+async function fetchOneHistory(symbolName) {
+  let history = await axios.get(`https://api.binance.com/api/v1/klines?symbol=${symbolName}&interval=1m`)
+  return history.data
+}
+
+async function collateData(data) {
+  let history = []
+  data.history.forEach(period => {
+    history.push({
+      'startTime': period[0],
+      'open': period[1],
+      'high': period[2],
+      'low': period[3],
+      'close': period[4],
+      'endTime': period[6]
+    })
+  })
+  let outputObject = {
+    'history': history,
+    'market': data.market
+  }
+  return outputObject
+}
+
+async function filter(markets, activeCurrency) {
+  try {
+    let outputArray = []
+    for (let i = 0; i < markets.length; i++) {
+      let market = markets[i]
+      let currentPrice = await fetchPrice(market)
+      if (market.market.indexOf(activeCurrency) === 0) {
+        if (
+          ema(market.history, 20, 'close') < ema(market.history, 50, 'close') && 
+          ema(market.history, 50, 'close') < ema(market.history, 200, 'close') && 
+          currentPrice < ema(market.history, 20, 'close')
+        ) {
+          outputArray.push(market)
+        } else {
+          // console.log(market.market)
+          // console.log(currentPrice)
+          // console.log(ema(market.history, 20, 'close'))
+          // console.log('\n')
+        }
+      } else {
+        if (
+          ema(market.history, 20, 'close') > ema(market.history, 50, 'close') && 
+          ema(market.history, 50, 'close') > ema(market.history, 200, 'close') && 
+          currentPrice > ema(market.history, 20, 'close')
+        ) {
+          outputArray.push(market)
+        } else {
+          // console.log(market.market)
+          // console.log(currentPrice)
+          // console.log(ema(market.history, 20, 'close'))
+          // console.log('\n')
+        }
+      }
+    }
+    return outputArray
+  } catch (error) {
+    console.log(error)
+  }
+
+}
+
+async function fetchPrice(market) {
+  try {
+    let symbolName = market.market.replace('/', '')
+    let rawPrice = await axios.get(`https://api.binance.com/api/v3/ticker/price?symbol=${symbolName}`) 
+    let price = parseFloat(rawPrice.data.price)
+    return price
+  } catch {
+    fetchPrice(market)
+  }
+}
+
+function ema(rawData, time, parameter) {
+  let data = extractData(rawData, parameter)
+  const k = 2/(time + 1)
+  let emaData = []
+  emaData[0] = data[0]
+  for (let i = 1; i < data.length; i++) {
+    let newPoint = (data[i] * k) + (emaData[i-1] * (1-k))
+    emaData.push(newPoint)
+  }
+  let currentEma = [...emaData].pop()
+  return +currentEma
+}
+
+function extractData(dataArray, key) {
+  let outputArray = []
+  dataArray.forEach(obj => {
+    outputArray.push(obj[key])
+  })
+  return outputArray
+}
+
 run();
