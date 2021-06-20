334050d542d86636e43439f6ed14e0d7354748ae
CodeKrakken <newholm1@hotmail.com>
2021-06-20 11:00:15 +0100

    Returns market names, narrowed by active currency

--- to-the-moon-2.js
+++ to-the-moon-2.js
@@ -32,7 +32,8 @@ async function run() {
 async function tick() {
   let activeCurrency = await getActiveCurrency()
   console.log(activeCurrency)
-  let markets = await getMarkets()
+  let markets = await getMarkets(activeCurrency)
+  console.log(markets)
 }
 
 async function getActiveCurrency() {
@@ -40,8 +41,112 @@ async function getActiveCurrency() {
   return sorted.pop()[0]
 }
 
-async function getMarkets() {
-
+async function getMarkets(currency) {
+  let markets = await fetchMarkets()
+  let marketNames = Object.keys(markets).filter(market => goodMarket(market, markets, currency))
+  let voluminousMarkets = await checkVolumes(marketNames)
+  console.log('Filtered by volume')
+  return voluminousMarkets
 }
 
+async function fetchMarkets() {
+  console.log(`Fetching overview at ${timeNow()}`)
+  let markets = await binance.load_markets()
+  return markets
+}
+
+function goodMarket(market, markets, currency) {
+  return markets[market].active 
+  && !market.includes('UP') 
+  && !market.includes('DOWN') 
+  && market.includes(currency) 
+  && !market.replace("USD", "").includes("USD")
+}
+
+function timeNow() {
+  let currentTime = Date.now()
+  let prettyTime = new Date(currentTime).toLocaleString()
+  return prettyTime
+}
+
+async function checkVolumes(marketNames) {
+  let voluminousMarkets = []
+  let symbolNames = marketNames.map(marketname => marketname = marketname.replace('/', ''))
+  let n = marketNames.length
+  let tallyObject = { 
+    assets: { total: 0, unique: 0 }, 
+    bases: { total: 0, unique: 0 } 
+  }
+  for (let i = 0; i < n; i ++) {
+    let symbolName = symbolNames[i]
+    let marketName = marketNames[i]
+    let asset = marketName.substring(0, marketName.indexOf('/'))
+    let base = marketName.substring(marketName.indexOf('/')+1)
+    tally(asset, base, tallyObject)
+    console.log(`Checking 24 hour volume of market ${i+1}/${n} - ${symbolName}`)
+    let response = await checkVolume(marketNames, i)
+    if (response === "Insufficient volume" || response === "No dollar comparison available") {
+      marketNames.splice(i, 1)
+      symbolNames.splice(i, 1)
+      i--
+      n--
+      console.log(response + '\n')
+    } else {
+      console.log(`Including ${marketName}\n`)
+      voluminousMarkets.push(marketName)
+    }
+  }
+  fs.appendFile('all market tally.txt', JSON.stringify(tallyObject), function(err) {
+    if (err) return console.log(err);
+  })
+  return voluminousMarkets
+}
+
+async function checkVolume(marketNames, i) {
+  let marketName = marketNames[i]
+  let asset = marketName.substring(0, marketName.indexOf('/'))
+  let dollarMarket = `${asset}/USDT`
+  if (marketNames.includes(dollarMarket)) {
+    let dollarSymbol = dollarMarket.replace('/', '')
+    let volumeDollarValue = await fetchDollarVolume(dollarSymbol)
+    if (volumeDollarValue < 50000000) { return "Insufficient volume"} 
+    if (volumeDollarValue === 'Invalid market') { return 'No dollar comparison available' }
+  } else {
+    return 'No dollar comparison available'
+  }
+  return 'Sufficient volume'
+}
+
+async function tally(asset, base, tallyObject) {
+  try{
+    if (Object.keys(tallyObject.assets).includes(asset)) {
+      tallyObject.assets[asset] ++
+    } else {
+      tallyObject.assets[asset] = 1
+      tallyObject.assets.unique ++
+    }
+    if (Object.keys(tallyObject.bases).includes(base)) {
+      tallyObject.bases[base] ++
+    } else {
+      tallyObject.bases[base] = 1
+      tallyObject.bases.unique ++
+    }
+    tallyObject.assets.total ++
+    tallyObject.bases.total ++
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+async function fetchDollarVolume(symbol) {
+  try {
+    let twentyFourHour = await axios.get(`https://api.binance.com/api/v3/ticker/24hr?symbol=${symbol}`)
+  let dollarPrice = parseFloat(twentyFourHour.data.weightedAvgPrice)
+  let totalVolume = parseFloat(twentyFourHour.data.volume)
+  volumeDollarValue = totalVolume * dollarPrice
+  return volumeDollarValue
+  } catch (error) {
+    return 'Invalid market'
+  }
+}
 run();
