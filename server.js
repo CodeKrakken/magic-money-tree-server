0b81f40728fc02c2884eb70738e86c0a7e4bac00
CodeKrakken <newholm1@hotmail.com>
2021-07-20 14:23:10 +0100

    Restore decent buying logic

--- server.js
+++ server.js
@@ -213,22 +213,22 @@ async function tick(wallet, goodMarketNames, currentMarket) {
     
       } else {
 
-        // let n = bulls.length
+        let n = bulls.length
 
-        // for (let i = 0; i < n; i ++) {
+        for (let i = 0; i < n; i ++) {
 
-          let bestMarket = bulls[0]
-          // let currentPrice = await fetchPrice(bestMarket.name)
+          let bestMarket = bulls[i]
+          let currentPrice = await fetchPrice(bestMarket.name)
 
-          // if (currentPrice > bestMarket.ema233) {
+          if (currentPrice > bestMarket.ema233) {
 
             // let response = await simulatedBuyOrder(wallet, bestMarket, goodMarketNames, currentMarket)
             let response = await liveBuyOrder(wallet, bestMarket, goodMarketNames, currentMarket)
             currentMarket = response['market']
             wallet = response['wallet']
             i = n
-          // }
-        // }
+          }
+        }
       }
     } else {
 
@@ -804,12 +804,12 @@ function displayMarkets(markets) {
 function getBulls(markets) {
 
   let bulls = markets.filter(market => 
-    // market.shape > 0 
-    // && 
+    market.shape > 0 
+    && 
     market.trend === 'up'
     && 
     market.pointLow > market.pointHigh
-  ) // Try picking a market where the point low is more recent than the point high - this should guarantee it is moving up
+  )
   return bulls
 }
 
