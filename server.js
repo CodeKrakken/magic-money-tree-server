ef648519ae04cca0ba398dbe3fd0d5c6fb5e41f2
CodeKrakken <newholm1@hotmail.com>
2023-02-20 19:57:22 +0000

    Attempt fix of client server communication error

--- server.js
+++ server.js
@@ -32,6 +32,14 @@ const periods = {
 const server = require('http').createServer();
 const io = require('socket.io')(server);
 
+io.on('connection', (socket) => {
+  console.log('A user connected.');
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected.');
+  });
+});
+
 // Send data to React app
 io.emit('data', { message: 'Hello from Node app!' });
 
